bd95872f5e0616f1e160ba111810a43a12ce1c36
Anurag Gupta <aganuraggupta786@gmail.com>
2026-01-12 20:56:29 +0530

    accordian toggle func added

--- src/pages/grocery/page.jsx
+++ src/pages/grocery/page.jsx
@@ -3,28 +3,24 @@ import { useProductCategoryApi } from "../../utils/helpers/useRestaurentApi";
 import { useState } from "react";
 
 const Grocery = () => {
-  const [viewProducts,setViewProducts] = useState(false)
-      const [endPoint, setEndPoint] = useState("");
+  const [viewProducts, setViewProducts] = useState("")
+  const [endPoint, setEndPoint] = useState("");
   const groceryList = useProductCategoryApi()
-  
-const handleTagClick =(tags)=>{
-  setEndPoint(tags)
-}
-console.log("view",viewProducts)
 
-console.log("endPoint",endPoint)
-  
+  const handleTagClick = (tags) => {
+    setEndPoint(tags)
+    setViewProducts((prev)=>prev === tags ? null : tags)
+  }
   return (
-    <div onClick={()=>  setViewProducts(!viewProducts)
-}>
+    <div>
       <h1 className="text-xl font-bold text-center m-4">Shop By Categories🛍️</h1>
-      {groceryList.map((tags,indx) => (
+      {groceryList.map((tags, indx) => (
         <div key={indx} className="mx-auto w-6/12">
-          <div onClick={()=>handleTagClick(tags)} className="flex mx-auto w-full justify-between p-4 m-2 shadow-lg font-bold ">
+          <div onClick={() => handleTagClick(tags)} className="flex mx-auto w-full justify-between p-4 m-2 shadow-lg font-bold ">
             <span>{tags.charAt(0).toUpperCase() + tags.slice(1)}</span>
             <button>🔻</button>
           </div>
-                   {viewProducts &&  <ProductByCategory endPoint={endPoint} />}
+          {viewProducts === tags && <ProductByCategory endPoint={endPoint} />}
         </div>
       ))
       }
