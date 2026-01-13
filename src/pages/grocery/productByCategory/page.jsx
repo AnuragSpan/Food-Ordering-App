c1e5a8ca2383eb35af824d530820701433e80e16
Anurag Gupta <aganuraggupta786@gmail.com>
2026-01-13 20:15:49 +0530

    grocery details page added along with API integration

--- src/pages/grocery/productByCategory/page.jsx
+++ src/pages/grocery/productByCategory/page.jsx
@@ -1,20 +1,34 @@
-import { useEffect, useState } from "react";
+import GroceryCard from "./GroceryCard";
+import { useGroceryByCategoryApi } from "../../../utils/helpers/useRestaurentApi";
 
-const ProductByCategory = ({endPoint}) => {
+const ProductByCategory = ({ endPoint }) => {
 
-    useEffect(() => {
-        const groceryDatas = async () => {
-            const res = await fetch(
-                `https://dummyjson.com/products/category/${endPoint}`
-            );
-            const json = await res.json();
-            console.log("Products:", json.products);
-        };
-        groceryDatas();
-    }, [endPoint]);
+    const grocData = useGroceryByCategoryApi(endPoint)
+        console.log("endpoint in page",endPoint)
+
+    console.log("grocery",grocData)
+    // const [grocery, setGrocery] = useState([]);
+   
+    // useEffect(() => {
+    //     const groceryDatas = async () => {
+    //         const res = await fetch(
+    //             `https://dummyjson.com/products/category/${endPoint}`
+    //         );
+    //         const json = await res.json();
+    //         console.log("json",json)
+    //         setGrocery(json?.products || []);
+    //     };
+
+    //     groceryDatas();
+    // }, [endPoint]);
     return (
-        <div>productByCategory</div>
-    )
-}
+        <div className="bg-gray-50 p-4 rounded-xl">
+         <GroceryCard  grocery={grocData}/>
+        </div>
+    );
+    
+};
 
-export default ProductByCategory
+
+
+export default ProductByCategory;
--- src/utils/helpers/useRestaurentApi.js
+++ src/utils/helpers/useRestaurentApi.js
@@ -21,7 +21,7 @@ export const useRestaurentApi = () => {
 
 export const useRestaurentDetailsApi = (resId) => {
     const [resDetails, setResDetails] = useState(null);
-console.log("resDetails",resDetails)
+    console.log("resDetails", resDetails)
     useEffect(() => {
         fetchResDetails()
     }, [resId])
@@ -36,15 +36,31 @@ console.log("resDetails",resDetails)
 
 // Product Category List
 
-export const useProductCategoryApi =()=>{
-  const [groceries, setGroceries] = useState([]);
-  useEffect(() => {
-    groceryData();
-  }, []);
- const groceryData = async () => {
-      const res = await fetch("https://dummyjson.com/products/category-list");
-      const json = await res.json();
-      setGroceries(json);
- }
- return groceries
+export const useProductCategoryApi = () => {
+    const [groceries, setGroceries] = useState([]);
+    useEffect(() => {
+        groceryData();
+    }, []);
+    const groceryData = async () => {
+        const res = await fetch("https://dummyjson.com/products/category-list");
+        const json = await res.json();
+        setGroceries(json);
+    }
+    return groceries
+}
+
+// Grocery Api
+
+export const useGroceryByCategoryApi = (endPoint)=>{
+    const [grocData,setGrocData] = useState([])
+
+    useEffect(()=>{
+        fetchGrocByCategoryData()
+    },[endPoint])
+    const fetchGrocByCategoryData = async()=>{
+        const res = await fetch(`https://dummyjson.com/products/category/${endPoint}`)
+        const data = await res.json()
+        setGrocData(data?.products)
+    }
+    return grocData;
 }
